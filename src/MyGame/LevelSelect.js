0e41c627ac4bf5164bce822aadb1b8bb6b994df6
Mousytongue <39573434+Mousytongue@users.noreply.github.com>
2019-03-16 00:22:04 -0700

    s

--- src/MyGame/LevelSelect.js
+++ src/MyGame/LevelSelect.js
@@ -12,7 +12,7 @@
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 function LevelSelect() {
     this.kUIButton = "assets/UI/button.png";
-    
+    this.kBG = "assets/DyeAssets/bg.png";
     // The camera to view the scene
     this.mCamera = null;
     this.World1Button = null;
@@ -24,10 +24,12 @@ gEngine.Core.inheritPrototype(LevelSelect, Scene);
 
 LevelSelect.prototype.loadScene = function () {
     gEngine.Textures.loadTexture(this.kUIButton);
+    gEngine.Textures.loadTexture(this.kBG);
 };
 
 LevelSelect.prototype.unloadScene = function () {
     gEngine.Textures.unloadTexture(this.kUIButton);
+    gEngine.Textures.unloadTexture(this.kBG);
     if(this.LevelSelect==="Level1"){
         gEngine.Core.startScene(new Level1());
     }
@@ -53,6 +55,9 @@ LevelSelect.prototype.initialize = function () {
     this.World1Button = new UIButton(this.kUIButton,this.World1Select,this,[650,450],[600,100],"World One",8,[1,1,1,1],[0,0,0,1]);
     this.World2Button = new UIButton(this.kUIButton,this.World2Select,this,[650,350],[750,100],"World Two",8,[1,1,1,1],[0,0,0,1]);
     this.GoBackButton = new UIButton(this.kUIButton,this.GoBackSelect,this,[650,250],[750,100],"Go Back",8,[1,1,1,1],[0,0,0,1]);
+    this.mBg = new TextureRenderable(this.kBG);
+    this.mBg.getXform().setSize(200,180);
+    this.mBg.getXform().setPosition(50,20);
 };
 
 // This is the draw function, make sure to setup proper drawing environment, and more
@@ -66,7 +71,7 @@ LevelSelect.prototype.draw = function () {
     this.World1Button.draw(this.mCamera);
     this.World2Button.draw(this.mCamera);
     this.GoBackButton.draw(this.mCamera);
-    //this.UIText.draw(this.mCamera);
+    this.mBg.draw(this.mCamera);
 };
 
 LevelSelect.prototype.update = function () {
--- src/MyGame/YouWin.js
+++ src/MyGame/YouWin.js
@@ -10,7 +10,7 @@
 /* find out more about jslint: http://www.jslint.com/help.html */
 
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
-function GameOver() {
+function YouWin() {
     this.kUIButton = "assets/UI/button.png";
     this.kBG = "assets/DyeAssets/bg.png";
     this.kFontImage = "assets/fonts/system-default-font.png";
@@ -21,17 +21,17 @@ function GameOver() {
     this.World2Button = null;
     this.GoBackButton = null;
 }
-gEngine.Core.inheritPrototype(GameOver, Scene);
+gEngine.Core.inheritPrototype(YouWin, Scene);
 
 
-GameOver.prototype.loadScene = function () {
+YouWin.prototype.loadScene = function () {
     gEngine.Textures.loadTexture(this.kUIButton);
     gEngine.Textures.loadTexture(this.kBG);
     gEngine.Textures.loadTexture(this.kFontImage);
     gEngine.Fonts.loadFont(this.kFont);
 };
 
-GameOver.prototype.unloadScene = function () {
+YouWin.prototype.unloadScene = function () {
     gEngine.Textures.unloadTexture(this.kUIButton);
     gEngine.Textures.unloadTexture(this.kBG);
     
@@ -40,7 +40,7 @@ GameOver.prototype.unloadScene = function () {
     }
 };
 
-GameOver.prototype.initialize = function () {
+YouWin.prototype.initialize = function () {
     // Step A: set up the cameras
     this.mCamera = new Camera(
         vec2.fromValues(50, 40), // position of the camera
@@ -53,9 +53,9 @@ GameOver.prototype.initialize = function () {
     
     this.GoBackButton = new UIButton(this.kUIButton,this.GoBackSelect,this,[650,250],[750,100],"Go Back",8,[1,1,1,1],[0,0,0,1]);
     
-    this.mGameText = new FontRenderable("Game Over");
+    this.mGameText = new FontRenderable("You Win");
     this.mGameText.setFont(this.kFont);
-    this._initText(this.mGameText, 10, 70, [1, 1, 1 ,1], 16);
+    this._initText(this.mGameText, 20, 70, [1, 1, 1 ,1], 16);
     
     this.mBg = new TextureRenderable(this.kBG);
     this.mBg.getXform().setSize(200,180);
@@ -64,7 +64,7 @@ GameOver.prototype.initialize = function () {
 
 // This is the draw function, make sure to setup proper drawing environment, and more
 // importantly, make sure to _NOT_ change any state.
-GameOver.prototype.draw = function () {
+YouWin.prototype.draw = function () {
     // Step A: clear the canvas
     gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
     
@@ -75,16 +75,16 @@ GameOver.prototype.draw = function () {
     this.mGameText.draw(this.mCamera);
 };
 
-GameOver.prototype.update = function () {
+YouWin.prototype.update = function () {
     this.GoBackButton.update();
 };
 
-GameOver.prototype.GoBackSelect= function(){
+YouWin.prototype.GoBackSelect= function(){
     this.LevelSelect="GoBack";
     gEngine.GameLoop.stop();
 };
 
-GameOver.prototype._initText = function (font, posX, posY, color, textH) {
+YouWin.prototype._initText = function (font, posX, posY, color, textH) {
     font.setColor(color);
     font.getXform().setPosition(posX, posY);
     font.setTextHeight(textH);
